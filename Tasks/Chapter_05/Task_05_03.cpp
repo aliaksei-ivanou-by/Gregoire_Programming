7830477cd06c6c896f8a820be9fced54af4ea32d
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2024-06-24 09:59:16 +0200

    Tasks/Chapter_05

---
+++ Tasks/Chapter_05/Task_05_03.cpp
@@ -0,0 +1,3 @@
+// It is better to avoid multiple inheritance.
+
+// In this case, each employee is actually a person, so it would be better to change the use of multiple inheritance to a simple single inheritance hierarchy as shown in Task_05_03.png.
---
+++ Tasks/Chapter_05/Task_05_04.cpp
@@ -0,0 +1,7 @@
+// See the class diagram in Task_05_04.png.
+
+// That diagram is just one example that includes a set of properties and behaviors.
+
+// The fact that a manager has a team of employees is modelled using a has - a relationship towards Employee.
+// The has - a relationship in this case is an aggregation and not a composition.
+// Also note that not all employees have a manager, for example, the CEO doesn't have any manager.
